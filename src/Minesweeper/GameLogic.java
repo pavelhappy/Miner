5b9e4ddaa3f6e48645bc3927a75fcaedbbcb8ace
Pavel Pogorelov <stosman@yandex.ru>
2016-07-11 01:17:41 +1000

    if bomb on 1st click

--- src/Minesweeper/GameLogic.java
+++ src/Minesweeper/GameLogic.java
@@ -8,6 +8,7 @@ public class GameLogic implements LogicInterface{
     int mines[][];
     int tile[][];
     int BOMBS;
+    int clickCount = 0;
     static Random ranrow = new Random();
     static Random rancol = new Random();
 
@@ -70,7 +71,19 @@ public class GameLogic implements LogicInterface{
     public void openCell(int row, int col)
     {
         int n=mines[row][col];
-        if (n ==9)
+        if ((n==9)&& (clickCount == 0)) {
+            tile[row][col]=0;
+            for (int i = 0; i < 1;) {
+                int randRow = ranrow.nextInt(ROWS);
+                int randCol = rancol.nextInt(COLS);
+                if (mines[randRow][randCol]!=9)
+                {
+                    mines[randRow][randCol] = 9;
+                    i++;
+                }
+            }
+        }
+        else if (n ==9)
         {
             for (int i = 0; i < ROWS; i++) {
                 for (int j = 0; j < COLS; j++) {
@@ -80,6 +93,11 @@ public class GameLogic implements LogicInterface{
                     }
                 }
             }
+            clickCount++;
+        }
+        else
+        {
+            clickCount++;
         }
         tile[row][col]=n;
 
--- src/Minesweeper/GraphicsInterface.java
+++ src/Minesweeper/GraphicsInterface.java
@@ -3,6 +3,13 @@ import java.awt.*;
 
 public interface GraphicsInterface {
     void createImagesMap();
+    /*
+    0 - empty cell
+    9 - mine cell
+    10 - closed cell
+    1-8 - number cell
+    */
+
     Image getImage(String filename);
 
 }
